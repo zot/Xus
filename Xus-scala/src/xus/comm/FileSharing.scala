41c3b584448c2a821d64969ec7ceda2c20818cf7
Bill Burdick <bill.burdick@gmail.com>
2010-01-24 09:20:36 +0200

    checkpoint

--- Xus-scala/src/xus/comm/FileSharing.scala
+++ Xus-scala/src/xus/comm/FileSharing.scala
@@ -8,20 +8,82 @@ object FileSharing extends ServiceFactory[FileSharingConnection,FileSharingMaste
 	def createMaster(topic: TopicMaster) = new FileSharingMaster(topic)
 }
 
+/**
+ * FileSharing - Xus peers share files as Git commits and keep track of them using tags
+ * the protocol has 4 messages:
+ * <pre>
+ * @see #storeTag() storeTag()
+ * @see #retrieveTag() retrieveTag()
+ * @see #storeChunk() storeChunk()
+ * @see #retrieveChunk() retrieveChunk()
+ * </pre>
+ * 
+ * {@code}
+ * 
+ * Peers store Git commits like using commit manifests and chunks
+ * a commit manifest lists all of the chunks for its commit and all
+ * of its trees and blobs.  The manifest is a file and is itself broken
+ * into chunks.  The list of manifest chunks is one chunk.  A tag is a
+ * chunk that contains a serialized git tag, along with the id of the
+ * manifest chunk list.  Peers store tags by name (i.e. an id computed
+ * based on the tag name) and chunks by id.
+ * 
+ * The procedure to retrieve a commit is:
+ * 
+ * <ol><li>retrieve the tag by name and store it in the local git repository
+ * <li>get the manifest chunk list ID from the tag
+ * <li>retrieve the manifest chunk list and store them in the local git repository
+ * <li>reconstruct the manifest from the chunks
+ * <li>retrieve any outstanding chunks in the manifest
+ * <li>reconstruct each tree node and blob from the chunks and store them in the local git repository
+ * </ol>
+ * 
+ */
 class FileSharingConnection(topic: TopicConnection) extends ServiceConnection(topic) {
 	override def receive(msg: DelegatedDHT, node: Node) {
 		for (n <- node.child) n match {
-		case Seq(<store>{data}</store>) => store(msg, bytesFor(data.mkString))
-		case Seq(<retrieve>{data}</retrieve>) => retrieve(msg)
+		case Seq(<store-tag/>) =>
+			for {
+				tagId <- strOpt(n, "tagId")
+				peerId <- strOpt(n, "taggerIdent")
+				message <- strOpt(n, "message")
+				tagType <- strOpt(n, "type")
+				name <- strOpt(n, "name")
+				objectId <- strOpt(n, "objectId")
+				chunkListId <- strOpt(n, "chunkListId")
+			} {
+				storeTag(msg, tagId, peerId, message, tagType, name, objectId, chunkListId)
+			}
+		case Seq(<retrieve-tag>{tagId}</retrieve-tag>) => retrieveTag(msg, tagId.mkString)
+		case Seq(<store-manifest-chunklist>{data}</store-manifest-chunklist>) => storeManifestChunkList(msg, bytesFor(data.mkString))
+		case Seq(<retrieve-manifest-chunklist>{data}</retrieve-manifest-chunklist>) => retrieveManifestChunkList(msg)
+		case Seq(<store-chunk>{data}</store-chunk>) => storeChunk(msg, bytesFor(data.mkString))
+		case Seq(<retrieve-chunk>{data}</retrieve-chunk>) => retrieveChunk(msg)
 		case _ =>
 		}
 	}
 
-	def store(msg: DelegatedDHT, data: Array[Byte]) {
+	def storeTag(msg: DelegatedDHT, tagId: String, peerId: String, messageId: String, tagType: String, name: String, objectId: String, chunkListId: String) {
 		msg.completed()
 	}
 
-	def retrieve(msg: DelegatedDHT) {
+	def retrieveTag(msg: DelegatedDHT, tagId: String) {
+		msg.completed()
+	}
+
+	def storeManifestChunkList(msg: DelegatedDHT, data: Array[Byte]) {
+		msg.completed()
+	}
+
+	def retrieveManifestChunkList(msg: DelegatedDHT) {
+		msg.completed()
+	}
+
+	def storeChunk(msg: DelegatedDHT, data: Array[Byte]) {
+		msg.completed()
+	}
+
+	def retrieveChunk(msg: DelegatedDHT) {
 		msg.completed()
 	}
 }
--- Xus-scala/testing/xus/testing/TestRepo.scala
+++ Xus-scala/testing/xus/testing/TestRepo.scala
@@ -2,13 +2,15 @@ package xus.testing
 
 import java.io.File
 import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.lib.Constants
+import org.eclipse.jgit.lib.ObjectId
 
 object TestRepo {
 	def main(args: Array[String]) {
 		val repo = new Repository(new File("/tmp/tst/.git"))
-		val obj = repo.openObject(ObjectId.fromString("ff74984cccd156a469afa7d9ab10e4777beb24"))
+		val obj = repo.openObject(ObjectId.fromString("78981922613b2afb6025042ff6bd878ac1994e85"))
 
-		println("type: "+obj.getType)
+		println("type: "+Constants.typeString(obj.getType))
 		println("done")
 	}
 }
