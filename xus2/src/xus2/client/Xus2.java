ac18b8edb1c526d923f58dc3eee688502d29def1
Shlomi v <shlomivaknin@gmail.com>
2010-10-06 00:57:33 +0200

    changed the request to textual, added a parser, and revised UI

--- xus2/src/xus2/client/Xus2.java
+++ xus2/src/xus2/client/Xus2.java
@@ -1,20 +1,24 @@
 package xus2.client;
 
+import xus2.shared.MalformedXusCommandException;
+import xus2.shared.XusCommand;
+import xus2.shared.XusParser;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.RequestBuilder;
-import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.RequestException;
-import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.ListBox;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -33,8 +37,6 @@ public class Xus2 implements EntryPoint {
 	 */
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 	
-	private String channelId = "none";
-	private boolean channelStarted = false;
 
 	final TextBox value = new TextBox();
 
@@ -43,153 +45,87 @@ public class Xus2 implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Label errorLabel = new Label();
-		System.err.println("ytytyyty " + channelId);
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel rootPanel = RootPanel.get("setCommand");
 		
-		HorizontalPanel unListenPanel = new HorizontalPanel();
+		System.err.println("ytytyyty ");
+		final XusClient xusClient = new XusClient();
 		
-		unListenPanel.setSize("271px", "29px");
-		
-		Button unlisten = new Button("New button");
-		unlisten.setText("unlisten");
-		unListenPanel.add(unlisten);
-		unlisten.setSize("100%", "27px");
-		
-		final TextBox unlisten_msgId = new TextBox();
-		unlisten_msgId.setVisibleLength(8);
-		unListenPanel.add(unlisten_msgId);
-		
-		final TextBox unlisten_key = new TextBox();
-		unlisten_key.setVisibleLength(8);
-		unlisten_key.setText("test");
-		unListenPanel.add(unlisten_key);
-		
-		
-		
-		HorizontalPanel setPanel = new HorizontalPanel();
-		rootPanel.add(setPanel, 92, 112);
-		setPanel.setSize("271px", "29px");
-		
-		Button set = new Button("New button");
-		setPanel.add(set);
-		set.setSize("100%", "27px");
-		set.setText("set");
-		
-		final TextBox msgId = new TextBox();
-		setPanel.add(msgId);
-		msgId.setVisibleLength(8);
-		
-		final TextBox key = new TextBox();
-		key.setText("test");
-		key.setVisibleLength(8);
-		setPanel.add(key);
-		
-		value.setText("ASS");
-		value.setVisibleLength(8);
-		setPanel.add(value);
-		
-		rootPanel.add(setPanel);
-		
-		HorizontalPanel listenPanel = new HorizontalPanel();
-		rootPanel.add(listenPanel);
-		listenPanel.setSize("271px", "29px");
-		
-		rootPanel.add(unListenPanel);
-		
-		Button listen = new Button("New button");
-		listen.setText("listen");
-		listenPanel.add(listen);
-		listen.setSize("100%", "27px");
-		
-		final TextBox listen_msgId = new TextBox();
-		listen_msgId.setVisibleLength(8);
-		listenPanel.add(listen_msgId);
-		
-		final TextBox listen_key = new TextBox();
-		listen_key.setText("test");
-		listen_key.setVisibleLength(8);
-		listenPanel.add(listen_key);
+		final DivElement output = DOM.getElementById("output").cast();
 
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+		final Button  clearBtn    = Button.wrap(DOM.getElementById("clear"));
 		
-		listBox = new ListBox();
-		rootPanel.add(listBox, 10, 115);
-		listBox.setSize("241px", "156px");
-		listBox.setVisibleItemCount(5);
-
-		unlisten.addClickHandler(new ClickHandler() {
+		final Button  listenBtn    = Button.wrap(DOM.getElementById("listen"));
+		final TextBox listenMsgId = TextBox.wrap(DOM.getElementById("listenMsgId"));
+		final TextBox listenKey   = TextBox.wrap(DOM.getElementById("listenKey"));
+		
+		final Button  unlistenBtn    = Button.wrap(DOM.getElementById("unlisten"));
+		final TextBox unlistenMsgId = TextBox.wrap(DOM.getElementById("unlistenMsgId"));
+		final TextBox unlistenKey   = TextBox.wrap(DOM.getElementById("unlistenKey"));
+		
+		final Button  setBtn    = Button.wrap(DOM.getElementById("set"));
+		final TextBox setMsgId = TextBox.wrap(DOM.getElementById("setMsgId"));
+		final TextBox setkeyVal[] = {
+				TextBox.wrap(DOM.getElementById("setKey1")), TextBox.wrap(DOM.getElementById("setVal1")),
+				TextBox.wrap(DOM.getElementById("setKey2")), TextBox.wrap(DOM.getElementById("setVal2")),
+				TextBox.wrap(DOM.getElementById("setKey3")), TextBox.wrap(DOM.getElementById("setVal3"))
+			};
+		
+		
+		output.setInnerHTML("Started");
+		
+		clearBtn.addClickHandler(new ClickHandler() {
+			
+			@Override
 			public void onClick(ClickEvent event) {
-				xusSend("/xus2/xus?cmd=unlisten&msgId="+unlisten_msgId.getText()+"&key="+unlisten_key.getText());
-				System.out.println("not listening to " + unlisten_key.getText());
+				output.setInnerHTML("Cleared");
 			}
 		});
-		listen.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				xusSend("/xus2/xus?cmd=listen&msgId="+listen_msgId.getText()+"&key="+listen_key.getText());
-				System.out.println("listening to " + key.getText());
-			}
-		});
-		set.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				xusSend("/xus2/xus?cmd=set&msgId="+msgId.getText()+"&key="+key.getText()+"&value="+value.getText());
-			}
-		});
-	}
-	
-	public void xusSend(String url) {
-		RequestBuilder req = new RequestBuilder(RequestBuilder.GET, url);
-		try {
-			if (channelStarted) req.setHeader("channelId", channelId);
-			req.sendRequest("", new RequestCallback() {
+		
+		xusClient.addIncomingHandler(new XusIncommingHandler(){
+			@Override
+			public void recievedMessage(String msg) {
+				output.setInnerHTML(output.getInnerHTML()+"<li>" + msg + "</li>");
+				System.out.println("(client) Got " + msg);
 				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					if (!channelStarted) {
-						channelId = response.getHeader("channelId");
-						System.out.println("client " + channelId);
-						channelStarted = true;
-						
-						Channel channel = ChannelFactory.createChannel(channelId);
-						channel.open(new SocketListener() {
-							
-							@Override
-							public void onOpen() {
-								
-							}
-							
-							@Override
-							public void onMessage(String message) {
-								recievedPushMessage(message);
-							}
-
-							
-						});
-						
-									
-						
-						System.out.println("=> set up channel " + channelId);
-					}
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					exception.printStackTrace();
+				try {
+					// parse the input
+					XusCommand p = XusParser.parse(msg);
 					
+					System.out.println(p);
+					
+				} catch (MalformedXusCommandException e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (RequestException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void recievedPushMessage(String message) {
-		System.out.println("Client: Push (" +channelId + ") : "+ message + " " );
-		//value.setText(message.split(" = ")[1]);
-		listBox.addItem(message);
+			}
+		});
 		
+
+		listenBtn.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				xusClient.xusListen(listenMsgId.getText(), listenKey.getText());
+			}
+		});
 		
+		unlistenBtn.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				xusClient.xusUnlisten(unlistenMsgId.getText(), unlistenKey.getText());
+			}
+		});
+		
+		setBtn.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				String keyVal[] = new String[setkeyVal.length];
+				
+				int i=0;
+				for (TextBox textBox : setkeyVal) {
+					keyVal[i++] = textBox.getText();
+				}
+				
+				xusClient.xusSet(setMsgId.getText(), keyVal);
+			}
+		});
 	}
 }
--- xus2/src/xus2/server/XusProtocolServlet.java
+++ xus2/src/xus2/server/XusProtocolServlet.java
@@ -11,6 +11,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import xus2.shared.KeyValue;
+import xus2.shared.MalformedXusCommandException;
+import xus2.shared.XusCommand;
+import xus2.shared.XusListen;
+import xus2.shared.XusParser;
+import xus2.shared.XusSet;
+import xus2.shared.XusUnlisten;
+
 import com.google.appengine.api.channel.ChannelFailureException;
 import com.google.appengine.api.channel.ChannelMessage;
 import com.google.appengine.api.channel.ChannelServiceFactory;
@@ -26,240 +34,165 @@ import com.google.appengine.api.datastore.Query.FilterOperator;
 @SuppressWarnings("serial")
 public class XusProtocolServlet extends HttpServlet {
 
-	enum XusProtocol {
-		set("msgId", "key", "value"),
-		listen("msgId", "key"),
-		unlisten("msgId", "key")
-		;
-		
-		String[] _args;
-		private XusProtocol(String... args) {
-			_args = args;
-		}
-		
-		public String[] getArgs() {return _args;};
-	};
+	
 	
 	AtomicInteger _ai = new AtomicInteger();
 	
 	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 	
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		XusProtocol cmd = null;
 		try {
-			cmd = XusProtocol.valueOf(req.getParameter("cmd"));
-		}catch (Exception e) {
-			System.err.println("Command " + req.getParameter("cmd") + " is not valid..");
-			return;
-		}
-		
-		
-		//HttpSession session = req.getSession(true);
-		//System.out.println("session = " + session);
-		 // Get a handle on the datastore itself
-
-		 /*
-		 // Lookup data by known key name
-		 Entity userEntity = datastore.get(KeyFactory.createKey("UserInfo", email));
-
-		 // Or perform a query
-		 Query query = new Query("Task", userEntity);
-		 query.addFilter("dueDate", Query.FilterOperator.LESS_THAN, today);
-		 for (Entity taskEntity : datastore.prepare(query).asIterable()) {
-		   if ("done".equals(taskEntity.getProperty("status"))) {
-		     datastore.delete(taskEntity);
-		   } else {
-		     taskEntity.setProperty("status", "overdue");
-		     datastore.put(taskEntity);
-		   }
-		 }
-		  */
-		
-		String channelId = req.getHeader("channelId");
-		
-		if (channelId == null) { // we need to create a new channel
-			System.out.println("Create a new one!");
-			String appId = "xusClient_"+_ai.getAndIncrement();
-			channelId = ChannelServiceFactory.getChannelService().createChannel(appId);
-			resp.setHeader("channelId", channelId);
-			System.out.println("Registering " + channelId + " => " + appId);
 			
-			Entity channelEnt = new Entity(KeyFactory.createKey("channelId", channelId));
-			channelEnt.setProperty("appId", appId);
-			datastore.put(channelEnt);
-		}
-		
-		String params[] = cmd.getArgs();
-		switch (cmd) {
-		case set:
-			String msgId = req.getParameter(params[0]);
-			String key = req.getParameter(params[1]);
-			String val = req.getParameter(params[2]);
+			String originalCmd = req.getParameter("cmd");
 			
-			Entity var = new Entity(KeyFactory.createKey("var", key));
-			var.setProperty("value", val);
+			XusCommand cmd = XusParser.parse(originalCmd);
 			
-			datastore.put(var);
+			String channelId = req.getHeader("channelId");
 			
-			int i = key.length();
-			
-			String nKey = key;
-			
-			// The same as below, but using queries
-			Query q = new Query("listener");
-			
-			ArrayList<Key> k = new ArrayList<Key>();
-			
-			do {
-				nKey = nKey.substring(0, i); 
-				k.add(KeyFactory.createKey("listener",nKey));
-			} while ((i = nKey.lastIndexOf("."))>0);
-			
-			q.addFilter("__key__", FilterOperator.IN, k);
-			
-			Iterator<Entity> iter = datastore.prepare(q).asIterable().iterator();
-			
-			while (iter.hasNext()) {
-				Entity listenerEnt = iter.next();
-				String curKey = listenerEnt.getKey().getName();
+			if (channelId == null) { // we need to create a new channel
+				System.out.println("Create a new one!");
+				String appId = "xusClient_"+_ai.getAndIncrement();
+				channelId = ChannelServiceFactory.getChannelService().createChannel(appId);
+				resp.setHeader("channelId", channelId);
+				System.out.println("Registering " + channelId + " => " + appId);
 				
-				List<String> listeners = (List<String>) listenerEnt.getProperty("channels");
+				Entity channelEnt = new Entity(KeyFactory.createKey("channelId", channelId));
+				channelEnt.setProperty("appId", appId);
+				datastore.put(channelEnt);
+			}
+			
+			if (cmd instanceof XusSet) {
+				XusSet set = (XusSet) cmd;
 				
-				if (listeners != null) {
-					System.out.println("holding " + listeners.size() + " listeners for key '" + curKey + "'");
+				String msgId = ((XusSet) cmd).getMsgId();
+				
+				List<KeyValue<String, String>> keysValues = set.getKeyValues();
+				
+				for (KeyValue<String, String> keyValue : keysValues) {
 					
-					Iterator<String> channelIter = listeners.iterator();
+					String key = keyValue.getKey();
+					String val = keyValue.getValue();
 					
-					while (channelIter.hasNext()) {
-						String channel = channelIter.next();
-						String appId;
-						try {
-							appId = (String) datastore.get(KeyFactory.createKey("channelId", channel)).getProperty("appId");
-							//
-							String dVal = null;
-							try {
-								dVal = (String) datastore.get(KeyFactory.createKey("var", curKey)).getProperty("value");
-							} catch (EntityNotFoundException e) {}
+					Entity var = new Entity(KeyFactory.createKey("var", key));
+					
+					datastore.put(var);
+					
+					int i = key.length();
+					
+					String nKey = key;
+					
+					// The same as below, but using queries
+					Query q = new Query("listener");
+					
+					ArrayList<Key> k = new ArrayList<Key>();
+					
+					do {
+						nKey = nKey.substring(0, i); 
+						k.add(KeyFactory.createKey("listener",nKey));
+					} while ((i = nKey.lastIndexOf("."))>0);
+					
+					q.addFilter("__key__", FilterOperator.IN, k);
+					
+					Iterator<Entity> iter = datastore.prepare(q).asIterable().iterator();
+					
+					while (iter.hasNext()) {
+						Entity listenerEnt = iter.next();
+						String curKey = listenerEnt.getKey().getName();
+						
+						List<String> listeners = (List<String>) listenerEnt.getProperty("channels");
+						
+						if (listeners != null) {
+							System.out.println("holding " + listeners.size() + " listeners for key '" + curKey + "'");
 							
-							try {
-								if (appId != null) ChannelServiceFactory.getChannelService().sendMessage(new ChannelMessage(appId, "set('"+msgId+"', '"+curKey + "', '" + dVal + "')"));
-							} catch (ChannelFailureException e) {
-								System.out.println("listener's channel is dead, removing. ");
-								channelIter.remove();
-								
-								if (listeners.isEmpty()) {
-									System.out.println("no more listeners, removing..");
-									datastore.delete(listenerEnt.getKey());
-								} else {
-									listenerEnt.setProperty("channels", listeners);
-									datastore.put(listenerEnt);
+							Iterator<String> channelIter = listeners.iterator();
+							
+							while (channelIter.hasNext()) {
+								String channel = channelIter.next();
+								String appId;
+								try {
+									appId = (String) datastore.get(KeyFactory.createKey("channelId", channel)).getProperty("appId");
+									
+									try {
+										if (appId != null) ChannelServiceFactory.getChannelService().sendMessage(new ChannelMessage(appId, "set('"+msgId+"','"+key+"','"+val+"')"));
+									} catch (ChannelFailureException e) {
+										System.out.println("listener's channel is dead, removing. ");
+										channelIter.remove();
+										
+										if (listeners.isEmpty()) {
+											System.out.println("no more listeners, removing..");
+											datastore.delete(listenerEnt.getKey());
+										} else {
+											listenerEnt.setProperty("channels", listeners);
+											datastore.put(listenerEnt);
+										}
+									}
+									
+								} catch (EntityNotFoundException e) {
+									e.printStackTrace();
 								}
 							}
-						
-						} catch (EntityNotFoundException e) {
-							e.printStackTrace();
+							
+						} else {
+							System.err.println("Got a null channel for listener on '" + nKey +"'. Wierd.");
+							continue;
 						}
+						
+						var.setProperty("value", val);
 					}
-					
-				} else {
-					System.err.println("Got a null channel for listener on '" + nKey +"'. Wierd.");
-					continue;
 				}
-			}
-			/*// same as above, without queries 
-			do {
-				nKey = nKey.substring(0, i);
+			} else if (cmd instanceof XusListen) {
+				XusListen listen = (XusListen) cmd;
 				
-				System.out.println("working on key: " + nKey);
+				String key = listen.getKey();
 				
-				Key listenerKey = KeyFactory.createKey("listener", nKey);
+				if (key.length() == 0) return;
 				
+				Key listenerKey = KeyFactory.createKey("listener", key);
+				
+				Entity dl = null;
+				List<String> channels = null;
+				try {
+					dl = datastore.get(listenerKey);
+					channels =  (List<String>) dl.getProperty("channels");
+					if (channels == null) channels = new ArrayList<String>(); 
+				}catch (EntityNotFoundException e) {
+					dl = new Entity(listenerKey);
+					channels = new ArrayList<String>();
+				}
+				
+				if (!channels.contains(channelId)) {
+					channels .add(channelId);
+					dl.setProperty("channels", channels);
+					datastore.put(dl);
+				}
+			} else if (cmd instanceof XusUnlisten) {
+				XusUnlisten unlisten = (XusUnlisten) cmd;
+				String key = unlisten.getKey();
+				
+				Key listenerKey = KeyFactory.createKey("listener", key);
 				try {
 					Entity listener = datastore.get(listenerKey);
-					
-					List<String> l = (List<String>) listener.getProperty("channels");
-					
-					
-					if (l != null) {
-						System.out.println("holding " + l.size() + " listeners for key '" + nKey + "'");
-						Iterator<String> iter = l.iterator();
+					List<String> channels = (List<String>) listener.getProperty("channels");
+					if (channels != null) { 
+						channels.remove(channelId);
+						listener.setProperty("channels", channels);
 						
-						while (iter.hasNext()) {
-							String channel = iter.next();
-							String appId;
-							try {
-								appId = (String) datastore.get(KeyFactory.createKey("channelId", channel)).getProperty("appId");
-								//System.out.println("Server: " +channel + " => " + appId);
-								String dVal = null;
-								try {
-									dVal = (String) datastore.get(KeyFactory.createKey("var", nKey)).getProperty("value");
-								} catch (EntityNotFoundException e) {}
-								
-								try {
-									if (appId != null) ChannelServiceFactory.getChannelService().sendMessage(new ChannelMessage(appId, "set('"+msgId+"', '"+nKey + "', '" + dVal + "')"));
-								} catch (ChannelFailureException e) {
-									System.out.println("listener's channel is dead, removing. ");
-									iter.remove();
-									listener.setProperty("channels", l);
-									datastore.put(listener);
-								}
-							
-							} catch (EntityNotFoundException e) {
-								e.printStackTrace();
-							}
+						if (channels.size() == 0) {
+							datastore.delete(listenerKey);
 						}
-					}					
+						else {
+							datastore.put(listener);
+						}
+					}
+				} catch (EntityNotFoundException e) {
 				}
-				catch (EntityNotFoundException e) {
-				}
-				
-			} while ((i = nKey.lastIndexOf(".")) > 0) ;
-			*/
-			break;
-		case listen: {
-			key = req.getParameter(params[1]);
-			
-			Key listenerKey = KeyFactory.createKey("listener", key);
-			
-			Entity dl = null;
-			List<String> channels = null;
-			try {
-				dl = datastore.get(listenerKey);
-				channels =  (List<String>) dl.getProperty("channels");
-				if (channels == null) channels = new ArrayList<String>(); 
-			}catch (EntityNotFoundException e) {
-				dl = new Entity(listenerKey);
-				channels = new ArrayList<String>();
 			}
+		
 			
-			if (!channels.contains(channelId)) {
-				channels .add(channelId);
-				dl.setProperty("channels", channels);
-				datastore.put(dl);
-			}
-		} break;
-		case unlisten: {
-			key = req.getParameter(params[1]);
-			
-			Key listenerKey = KeyFactory.createKey("listener", key);
-			try {
-				Entity listener = datastore.get(listenerKey);
-				List<String> channels = (List<String>) listener.getProperty("channels");
-				channels.remove(channelId);
-				listener.setProperty("channels", channels);
-				datastore.put(listener);
-			} catch (EntityNotFoundException e) {
-			}
-		} break;
-		default:
-			break;
+		} catch (MalformedXusCommandException e) {
+			e.printStackTrace();
 		}
-		
-		//datastore.get(KeyFactory.req.getParameter(params[1]));
-		
-	}
 	
+	}
 }
